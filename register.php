e1856da51eece5cd54a0a95537754073253c5f03
Reef <147044717+Reef-ldn@users.noreply.github.com>
2025-03-01 14:22:14 +0000

    Implemented multiple profile functionality (Added Scouts, managers and fans)

--- register.php
+++ register.php
@@ -1,9 +1,19 @@
-<!--Simple Registration Form - Creates an account-->
+<!-- Registration Form - Creates an account-->
 <form action = "register.php" method = "POST">
   <h1>Sign Up</h1>
   Username: <input type = "text" name = "username">  <br>
   Email: <input type = "email" name = "email">   <br>
   Password: <input type = "password" name="password">    <br>
+
+  <!--Account Type Selection -->
+  <label>Account Type:</label>
+  <select name = "user_type" required>
+    <option value = "player">Player</option>
+    <option value = "manager">Manager</option>
+    <option value = "scout">Scout</option>
+    <option value = "fan">Fan</option>
+  </select><br><br>
+
   <button type="submit" name="submit">Register</button>
 </form>
 
@@ -13,6 +23,9 @@ if(isset($_POST['submit'])) {       //checks if the form was submitted through t
   $username = $_POST['username'];
   $email = $_POST['email'];
   $password = $_POST['password'];
+
+  //Get the user's account type
+  $user_type = $_POST['user_type'];
 
   //Connects the mySQL database
   $conn = new mysqli("localhost", "root", "", "projectx_db");
@@ -25,11 +38,11 @@ if(isset($_POST['submit'])) {       //checks if the form was submitted through t
   $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
   //Inserts the new user into the 'users' table as a column
-  $user_type = 'player';    //for now this will be set to player for the sake of the MVP
+  //$user_type = 'player';    //for now this will be set to player for the sake of the MVP
   $sql = 
   "INSERT INTO users (username, email, password, created_at, user_type)
   VALUES ('$username', '$email', '$hashed_password', NOW(), '$user_type')";
-  $result = $conn->query($sql);
+  $result = $conn->query($sql); 
 
   //check if the insertion worked
   if($result == TRUE) {
@@ -43,16 +56,28 @@ if(isset($_POST['submit'])) {       //checks if the form was submitted through t
       $conn->query($playerSql);
      }
 
+     //If the user_type is 'scout', insert a row in the 'scouts' table in the db
+     if($user_type == 'scout') {
+      $scoutSql = "INSERT INTO scouts (user_id)
+              VALUES ('$newUserId')";
+      $conn->query($scoutSql);
+     }
+
+
+     //If the user_type is 'manager', insert a row in the 'managers' table in the db
+     if($user_type == 'manager') {
+      $managerSql = "INSERT INTO managers (user_id, age)
+              VALUES ('$newUserId', 0)";
+      $conn->query($managerSql);
+     }
+
+
       //Gives Feedback to the user
       //if($conn->query($sql) === TRUE){    // "if the sql command works then...."
       echo "Registration Successful!";
     } else {
         echo "Error: " . $conn->error;
     }
- 
-
- 
-  
 
 
   $conn->close();   //closes the database connection
